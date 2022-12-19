150382d680a034235773c6df573c7830d0ddaf56
ehdghk154 <ehdghk154@naver.com>
2022-12-19 20:15:22 +0900

    create project

---
+++ board-springboot/src/test/java/com/mysite/shin/BoardSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mysite.shin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
